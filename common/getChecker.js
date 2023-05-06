01b891696f394d15f5407c42b0d764dfd3af3b37
karim mansour <don.karimmansour@gmail.com>
2023-05-06 14:29:13 +0100

    jjj

--- common/getChecker.js
+++ common/getChecker.js
@@ -2,6 +2,22 @@ const FC = filter => {
     return filter ? JSON.parse(filter) : {}
 }
 
+const QC = (s, q) => {
+
+    const adminFilter = { fullname, email }
+    const userFilter = { fullname, email, phone, address }
+    const postFilter = { fullname, email }
+    const categoryFilter = { fullname, email }
+    const likeFilter = { fullname, email}
+    const commentFilter = { fullname, email }
+    const sliderFilter = { fullname, email }
+    const notificationFilter = { title, description }
+
+
+
+    return q ? q && s === "admin" : {}
+}
+
 const OC = (skip, limit, sort) => {
      let options = {}
 
